f1843541c9417c5d800a7b247e01d15bf448a671
guiwoliveira <guilherme77oliveira@gmail.com>
2023-10-28 01:10:21 -0300

    alter: formulario

--- src/components/pages/Cadastro.js
+++ src/components/pages/Cadastro.js
@@ -10,27 +10,27 @@ function Cadastro() {
             <form className={styles.form}>
                 <div>
                     <label>Tipo de vaga</label>
-                    <input type='text' name='tipo' placeholder='Júnior, estágio, senior...' />
+                    <input type='text' name='tipo' placeholder='ex.: Júnior, estágio, senior...' />
                 </div>         
                 <div>
                     <label>Descrição</label>
-                    <textarea type='text' name='descricao' placeholder='Nós gostariamos...' />
+                    <textarea type='text' name='descricao' placeholder='ex.: Nós gostariamos...' />
                 </div>         
                 <div>
                     <label>Cargo</label>
-                    <input type='text' name='cargo' placeholder='Programador, auxiliar...' />
+                    <input type='text' name='cargo' placeholder='ex.: Programador, auxiliar...' />
                 </div>         
                 <div>
                     <label>Local</label>
-                    <input type='text' name='local' placeholder='R. Nome, 123' />
+                    <input type='text' name='local' placeholder='ex.: R. Nome, 123' />
                 </div>         
                 <div>
                     <label>Turno</label>
-                    <input type='text' name='turno' placeholder='Diurno, noturno...' />
+                    <input type='text' name='turno' placeholder='ex.: Diurno, noturno...' />
                 </div>         
                 <div>
                     <label>Salário</label>
-                    <input type='text' name='salario' placeholder='xxxxx,xx' />
+                    <input type='number' name='salario' placeholder='ex.: xxxxx,xx' />
                 </div>
                 <div className={styles.form_button}>
                     <LinkButton to="/vagas" text="Cadastrar" />
